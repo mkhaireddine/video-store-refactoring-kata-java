9924758ae8874e6746a9c854fd9a8740f36610b3
KHAIREDDINE Mohamed <mohamed.khaireddine@gmail.com>
2021-03-27 12:02:46 +0100

    move getAmount method to Movie class

--- src/main/java/com/kata/videostore/Movie.java
+++ src/main/java/com/kata/videostore/Movie.java
@@ -26,4 +26,26 @@ public class Movie {
     return title;
   }
 
+  double getAmount(int daysRented) {
+    double thisAmount = 0;
+    // determines the amount for each line
+    switch (getPriceCode()) {
+      case REGULAR:
+        thisAmount += 2;
+        if (daysRented > 2) {
+          thisAmount += (daysRented - 2) * 1.5;
+        }
+        break;
+      case NEW_RELEASE:
+        thisAmount += daysRented * 3;
+        break;
+      case CHILDRENS:
+        thisAmount += 1.5;
+        if (daysRented > 3) {
+          thisAmount += (daysRented - 3) * 1.5;
+        }
+        break;
+    }
+    return thisAmount;
+  }
 }
--- src/main/java/com/kata/videostore/Rental.java
+++ src/main/java/com/kata/videostore/Rental.java
@@ -19,39 +19,16 @@ public class Rental {
   private int daysRented;
 
   String getTitle() {
-    return getMovie().getTitle();
+    return movie.getTitle();
   }
 
   double getAmount() {
-    return getAmount(daysRented);
-  }
-
-  double getAmount(int daysRented) {
-    double thisAmount = 0;
-    // determines the amount for each line
-    switch (getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-        thisAmount += 2;
-        if (daysRented > 2) {
-          thisAmount += (daysRented - 2) * 1.5;
-        }
-        break;
-      case Movie.NEW_RELEASE:
-        thisAmount += daysRented * 3;
-        break;
-      case Movie.CHILDRENS:
-        thisAmount += 1.5;
-        if (daysRented > 3) {
-          thisAmount += (daysRented - 3) * 1.5;
-        }
-        break;
-    }
-    return thisAmount;
+    return movie.getAmount(daysRented);
   }
 
   int getFrequentRenterPoints() {
     int frequentRenterPoints =1;
-    if (getMovie().getPriceCode() == Movie.NEW_RELEASE
+    if (movie.getPriceCode() == Movie.NEW_RELEASE
         && daysRented > 1) {
       frequentRenterPoints++;
     }
