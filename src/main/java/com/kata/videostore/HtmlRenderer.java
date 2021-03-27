9ec09c2f5a697f3c47315ba4ad293b71ab02cadd
KHAIREDDINE Mohamed <mohamed.khaireddine@gmail.com>
2021-03-27 12:21:34 +0100

    create HtmlRenderer class

---
+++ src/main/java/com/kata/videostore/HtmlRenderer.java
@@ -0,0 +1,26 @@
+package com.kata.videostore;
+
+import java.util.List;
+
+public class HtmlRenderer implements Renderer {
+
+  @Override
+  public String rentalLines(List<Rental> rentals) {
+    return null;
+  }
+
+  @Override
+  public String rentalLine(String title, double amount) {
+    return null;
+  }
+
+  @Override
+  public String footer(double totalAmount, int frequentRenterPoints) {
+    return null;
+  }
+
+  @Override
+  public String header(String name) {
+    return null;
+  }
+}
