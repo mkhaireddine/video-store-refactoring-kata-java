9495538dd37372be9eeb349f0bc7938c6003b013
KHAIREDDINE Mohamed <mohamed.khaireddine@gmail.com>
2021-04-03 12:11:42 +0200

    use java 15

--- src/test/java/com/kata/videostore/VideoStoreTest.java
+++ src/test/java/com/kata/videostore/VideoStoreTest.java
@@ -20,10 +20,12 @@ public class VideoStoreTest {
     customer.addRental(new Rental(Movie.newRelease("The Cell"), 3));
 
     assertThat(customer.statement()).isEqualTo(
-        "Rental Record for Fred\n"
-            + "\tThe Cell\t9.0\n"
-            + "You owed 9.0\n"
-            + "You earned 2 frequent renter points\n");
+        """
+            Rental Record for Fred
+            \tThe Cell\t9.0
+            You owed 9.0
+            You earned 2 frequent renter points
+            """);
   }
 
   @Test
@@ -32,11 +34,13 @@ public class VideoStoreTest {
     customer.addRental(new Rental(Movie.newRelease("The Tigger Movie"), 3));
 
     assertThat(customer.statement()).isEqualTo(
-        "Rental Record for Fred\n"
-            + "\tThe Cell\t9.0\n"
-            + "\tThe Tigger Movie\t9.0\n"
-            + "You owed 18.0\n"
-            + "You earned 4 frequent renter points\n");
+        """
+            Rental Record for Fred
+            \tThe Cell\t9.0
+            \tThe Tigger Movie\t9.0
+            You owed 18.0
+            You earned 4 frequent renter points
+            """);
   }
 
   @Test
@@ -44,10 +48,12 @@ public class VideoStoreTest {
     customer.addRental(new Rental(Movie.children("The Tigger Movie"), 3));
 
     assertThat(customer.statement()).isEqualTo(
-        "Rental Record for Fred\n"
-            + "\tThe Tigger Movie\t1.5\n"
-            + "You owed 1.5\n"
-            + "You earned 1 frequent renter points\n");
+        """
+            Rental Record for Fred
+            \tThe Tigger Movie\t1.5
+            You owed 1.5
+            You earned 1 frequent renter points
+            """);
   }
 
   @Test
@@ -55,10 +61,12 @@ public class VideoStoreTest {
     customer.addRental(new Rental(Movie.children("The Tigger Movie"), 4));
 
     assertThat(customer.statement()).isEqualTo(
-        "Rental Record for Fred\n"
-            + "\tThe Tigger Movie\t3.0\n"
-            + "You owed 3.0\n"
-            + "You earned 1 frequent renter points\n");
+        """
+            Rental Record for Fred
+            \tThe Tigger Movie\t3.0
+            You owed 3.0
+            You earned 1 frequent renter points
+            """);
   }
 
   @Test
@@ -68,12 +76,14 @@ public class VideoStoreTest {
     customer.addRental(new Rental(Movie.regular("Eraserhead"), 3));
 
     assertThat(customer.statement()).isEqualTo(
-        "Rental Record for Fred\n"
-            + "\tPlan 9 from Outer Space\t2.0\n"
-            + "\t8 1/2\t2.0\n"
-            + "\tEraserhead\t3.5\n"
-            + "You owed 7.5\n"
-            + "You earned 3 frequent renter points\n");
+        """
+            Rental Record for Fred
+            \tPlan 9 from Outer Space\t2.0
+            \t8 1/2\t2.0
+            \tEraserhead\t3.5
+            You owed 7.5
+            You earned 3 frequent renter points
+            """);
   }
 
   @Test
@@ -85,14 +95,16 @@ public class VideoStoreTest {
     customer.addRental(new Rental(Movie.regular("8 1/2"), 2));
     customer.addRental(new Rental(Movie.regular("Eraserhead"), 3));
 
-    final String expected = "<h1>Rental Record for <em>martin</em></h1>\n"
-        + "<table>\n"
-        + "  <tr><td>Plan 9 from Outer Space</td><td>2.0</td></tr>\n"
-        + "  <tr><td>8 1/2</td><td>2.0</td></tr>\n"
-        + "  <tr><td>Eraserhead</td><td>3.5</td></tr>\n"
-        + "</table>\n"
-        + "<p>Amount owed is <em>7.5</em></p>\n"
-        + "<p>You earned <em>3</em> frequent renter points</p>\n";
+    final String expected = """
+        <h1>Rental Record for <em>martin</em></h1>
+        <table>
+          <tr><td>Plan 9 from Outer Space</td><td>2.0</td></tr>
+          <tr><td>8 1/2</td><td>2.0</td></tr>
+          <tr><td>Eraserhead</td><td>3.5</td></tr>
+        </table>
+        <p>Amount owed is <em>7.5</em></p>
+        <p>You earned <em>3</em> frequent renter points</p>
+        """;
 
 //    assertThat(customer.statementHTML()).isEqualTo(expected);
   }
@@ -106,10 +118,12 @@ public class VideoStoreTest {
     // customer.addRental(new Rental(new Movie("Promo 3", Movie.PROMO), 3));
 
     assertThat(customer.statement()).isEqualTo(
-        "Rental Record for Fred\n"
-            + "\tPromo 3\t1.5\n"
-            + "You owed 1.5\n"
-            + "You earned 0 frequent renter points\n");
+        """
+            Rental Record for Fred
+            \tPromo 3\t1.5
+            You owed 1.5
+            You earned 0 frequent renter points
+            """);
   }
 
   @Test
@@ -120,12 +134,14 @@ public class VideoStoreTest {
 //    customer.addRental(new Rental(new Movie("Promo 3", Movie.PROMO), 3));
 
     assertThat(customer.statement()).isEqualTo(
-        "Rental Record for Fred\n"
-            + "\tPromo 1\t0.5\n"
-            + "\tPromo 2\t1.0\n"
-            + "\tPromo 3\t1.5\n"
-            + "You owed 3.0\n"
-            + "You earned 0 frequent renter points\n");
+        """
+            Rental Record for Fred
+            \tPromo 1\t0.5
+            \tPromo 2\t1.0
+            \tPromo 3\t1.5
+            You owed 3.0
+            You earned 0 frequent renter points
+            """);
   }
 
 }
