0c64825f8b53aaa3faefd327d5996aed9bc7630e
KHAIREDDINE Mohamed <mohamed.khaireddine@gmail.com>
2021-04-03 12:20:12 +0200

    implement html footer

--- src/main/java/com/kata/videostore/HtmlRenderer.java
+++ src/main/java/com/kata/videostore/HtmlRenderer.java
@@ -11,7 +11,7 @@ public class HtmlRenderer implements Renderer {
   public String rentalLines(List<Rental> rentals) {
     return rentals.stream()
         .map(rental -> rentalLine(rental.getTitle(), rental.getAmount()))
-        .collect(joining("\n","<table>\n","\n</table>\n"));
+        .collect(joining("\n", "<table>\n", "\n</table>\n"));
   }
 
   @Override
@@ -27,6 +27,9 @@ public class HtmlRenderer implements Renderer {
 
   @Override
   public String footer(double totalAmount, int frequentRenterPoints) {
-    return String.format("You owed %s\nYou earned %d frequent renter points\n", totalAmount, frequentRenterPoints);
+    return String.format("""
+        <p>Amount owed is <em>%s</em></p>
+        <p>You earned <em>%d</em> frequent renter points</p>
+        """, totalAmount, frequentRenterPoints);
   }
 }
